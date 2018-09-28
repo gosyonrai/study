91152b31148d913f87a4c35a2b8e67d1746d0af4
呉 <go_syonrai@densan-s.co.jp>
2018-09-28 16:34:57 +0900

    開始

--- study/src/main/java/jp/co/study/HelloAppEngine.java
+++ study/src/main/java/jp/co/study/HelloAppEngine.java
@@ -16,7 +16,7 @@ public class HelloAppEngine extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
-
+	  System.out.println("開始");
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
