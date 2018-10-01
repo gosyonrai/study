6d347f6bbedf88fa0fbdc15d6a7622718109cfba
ジョ <jyo_kaikoku@densan-s.co.jp>
2018-10-01 14:52:01 +0900

    conflictテスト

--- study/src/main/java/jp/co/study/HelloAppEngine.java
+++ study/src/main/java/jp/co/study/HelloAppEngine.java
@@ -16,6 +16,7 @@ public class HelloAppEngine extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
+	  System.out.println("conflictテスト!");
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().print("Hello App Engine!!\r\n");
